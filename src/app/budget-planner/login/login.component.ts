b126d684ec7e3e759dd5dde37a44874e51b6cc7f
Dmitriy Kharchenko <dm.kharchenko1988@gmail.com>
2024-03-30 21:09:18 +0300

    created budget planner with angular

---
+++ src/app/budget-planner/login/login.component.ts
@@ -0,0 +1,31 @@
+import {Component} from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm: any;
+  registerForm: any;
+  activeForm: 'login' | 'register' = 'login';
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+}
